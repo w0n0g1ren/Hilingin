8adec04e0a32264b5ef07ba82cbe8f79d58afd46
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2022-12-25 00:35:45 +0700

    edit XML lagi bre anjay mabar wohoo

--- app/src/main/java/com/example/hilingin/SplashScreen.kt
+++ app/src/main/java/com/example/hilingin/SplashScreen.kt
@@ -19,7 +19,7 @@ class SplashScreen : AppCompatActivity() {
 
         // Handler().postDelayed({
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, TransaksiActivity::class.java)
             startActivity(intent)
             finish()
             }, 3000)
