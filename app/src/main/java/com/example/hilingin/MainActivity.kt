5cbe8d01e48d70c4ee89505f9d3f4aa2d429447d
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2022-12-29 01:51:17 +0700

    tahun baru penake ngopo

--- app/src/main/java/com/example/hilingin/MainActivity.kt
+++ app/src/main/java/com/example/hilingin/MainActivity.kt
@@ -3,17 +3,20 @@ package com.example.hilingin
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val RecyclerView = findViewById<RecyclerView>(R.id._imageRecycleView)
-        RecyclerView.layoutManager = LinearLayoutManager(this)
-        RecyclerView.setHasFixedSize(true)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
